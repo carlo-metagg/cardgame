1e3235906c72c3fa0d0e0f2c1ba02a1d88fb079c
John Carlo Ariola <ariola.johncarlo.projects@gmail.com>
2021-12-27 10:03:43 +0800

    Battle System prototype Implemented basic spawning directly on player hand Lerping implemented on card drag Implemented prototype for centering all cards currently in hand resolves 9

--- Assets/Scripts/Minion/MinionBehaviour.cs
+++ Assets/Scripts/Minion/MinionBehaviour.cs
@@ -1,10 +1,11 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.EventSystems;
 
-using TMPro;
-
-public class MinionBehaviour : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+public class MinionBehaviour : MonoBehaviour
 {
     [SerializeField] private MinionCardData cardData;
 
@@ -17,11 +18,17 @@ public class MinionBehaviour : MonoBehaviour, IBeginDragHandler, IDragHandler, I
     [SerializeField] private TextMeshProUGUI health;
     [SerializeField] private TextMeshProUGUI manaCost;
 
-    [SerializeField] private float scaleMultiplier = 1;
+    [SerializeField] private float lerpMultiplier = 20;
+    [SerializeField] private float returnToHandDuration = 0.5f;
+
+    private Vector3 delta;
+    private Vector3 initialPosition;
 
     void Start()
     {
         InitializeCard();
+
+        initialPosition = Vector3.zero;
     }
 
     private void InitializeCard()
@@ -34,25 +41,37 @@ public class MinionBehaviour : MonoBehaviour, IBeginDragHandler, IDragHandler, I
         attack.text = cardData.Attack.ToString();
         health.text = cardData.Health.ToString();
         manaCost.text = cardData.ManaCost.ToString();
-
-        transform.localScale *= scaleMultiplier;
     }
 
-    public MinionCardData CardData { set => cardData = value; }
+    public MinionCardData CardData { get => cardData;  set => cardData = value; }
+    public  Vector3 InitialPosition { get => initialPosition; }
 
-    public void OnBeginDrag(PointerEventData eventData)
+    public void SetCurrentPositionAsInitialPosition() => initialPosition = transform.position;
+
+    private void OnMouseDrag()
     {
-        print("begin drag");
-        transform.SetParent(transform.root);
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        transform.position = Vector2.Lerp(transform.position, mousePosition, Time.deltaTime * lerpMultiplier);
     }
 
-    public void OnDrag(PointerEventData eventData)
+    private void OnMouseUp()
     {
-        transform.position = Input.mousePosition;
+        StartCoroutine(ReturnToInitial());
     }
 
-    public void OnEndDrag(PointerEventData eventData)
+    private IEnumerator ReturnToInitial()
     {
-        print("End drag");
+        Vector3 startingPos = transform.position;
+        Vector3 finalPos = initialPosition;
+        float elapsedTime = 0;
+
+        while (finalPos != transform.position)
+        {
+            float lerpTravelPercentage = elapsedTime / returnToHandDuration;
+            transform.position = Vector3.Lerp(startingPos, finalPos, lerpTravelPercentage);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
     }
 }
